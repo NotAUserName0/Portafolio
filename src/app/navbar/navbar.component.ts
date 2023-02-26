1cc8b84e1734f5af1e598cef547059c6b456d4be
Fermin Jimenez <dill2897@gmail.com>
2023-02-25 18:36:38 -0600

    add

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -7,4 +7,49 @@ import { Component } from '@angular/core';
 })
 export class NavbarComponent {
 
+  isDark:boolean = false;
+
+  dark(){
+    this.isDark = !this.isDark;
+    if(this.isDark){
+      document.body.style.color = "white";
+      document.body.style.backgroundColor = "black";
+      var nav = document.getElementById('navbar');
+      nav.classList.remove('bg-white');
+      nav.classList.add('bg-black');
+      nav.classList.add('navbar-dark')
+    }else{
+      document.body.style.color = "black";
+      document.body.style.backgroundColor = "white";
+      var nav = document.getElementById('navbar');
+      nav.classList.add('bg-white');
+      nav.classList.remove('bg-black');
+      nav.classList.remove('navbar-dark')
+    }
+  }
+
+  darkPhone(){
+    this.isDark = !this.isDark;
+    if(this.isDark){
+      document.body.style.color = "white";
+      document.body.style.backgroundColor = "black";
+      var nav = document.getElementById('navbar-phone');
+      nav.classList.remove('bg-white');
+      nav.classList.add('bg-black');
+      nav.classList.add('navbar-dark')
+      var canvas = document.getElementById('offcanvasNavbar');
+      canvas.classList.add('text-bg-dark');
+    }else{
+      document.body.style.color = "black";
+      document.body.style.backgroundColor = "white";
+      var nav = document.getElementById('navbar-phone');
+      nav.classList.add('bg-white');
+      nav.classList.remove('bg-black');
+      nav.classList.remove('navbar-dark')
+      var canvas = document.getElementById('offcanvasNavbar');
+      canvas.classList.remove('text-bg-dark');
+
+    }
+  }
+
 }
