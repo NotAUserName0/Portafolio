1f7e38db7f581a6281166fb22d60553b39d67a05
Fermin Jimenez <dill2897@gmail.com>
2023-02-25 23:07:46 -0600

    add

--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -7,4 +7,28 @@ import { Component } from '@angular/core';
 })
 export class ProyectsComponent {
 
+
+  enter(x){
+    if(x == 1){
+      document.getElementById('p1').style.setProperty("display","block","important");
+    }else if(x == 2){
+      document.getElementById('p2').style.setProperty("display","block","important");
+    }else if(x == 3){
+      document.getElementById('p3').style.setProperty("display","block","important");
+    }else{
+      document.getElementById('p4').style.setProperty("display","block","important");
+    }
+  }
+
+  exit(x){
+    if(x == 1){
+      document.getElementById('p1').style.setProperty("display","none","important");
+    }else if(x == 2){
+      document.getElementById('p2').style.setProperty("display","none","important");
+    }else if(x == 3){
+      document.getElementById('p3').style.setProperty("display","none","important");
+    }else{
+      document.getElementById('p4').style.setProperty("display","none","important");
+    }
+  }
 }
