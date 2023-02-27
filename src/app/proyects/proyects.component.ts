b3e511ca006d5c1966200827f314d8bb89ec78f5
Fermin Jimenez <dill2897@gmail.com>
2023-02-27 11:39:55 -0600

    add

--- src/app/proyects/proyects.component.ts
+++ src/app/proyects/proyects.component.ts
@@ -10,25 +10,33 @@ export class ProyectsComponent {
 
   enter(x){
     if(x == 1){
-      document.getElementById('p1').style.setProperty("display","block","important");
+      document.getElementById('p1').classList.remove('hide');
+      document.getElementById('p1').classList.add('show');
     }else if(x == 2){
-      document.getElementById('p2').style.setProperty("display","block","important");
+      document.getElementById('p2').classList.remove('hide');
+      document.getElementById('p2').classList.add('show');
     }else if(x == 3){
-      document.getElementById('p3').style.setProperty("display","block","important");
+      document.getElementById('p3').classList.remove('hide');
+      document.getElementById('p3').classList.add('show');
     }else{
-      document.getElementById('p4').style.setProperty("display","block","important");
+      document.getElementById('p4').classList.remove('hide');
+      document.getElementById('p4').classList.add('show');
     }
   }
 
   exit(x){
     if(x == 1){
-      document.getElementById('p1').style.setProperty("display","none","important");
+      document.getElementById('p1').classList.remove('show');
+      document.getElementById('p1').classList.add('hide');
     }else if(x == 2){
-      document.getElementById('p2').style.setProperty("display","none","important");
+      document.getElementById('p2').classList.remove('show');
+      document.getElementById('p2').classList.add('hide');
     }else if(x == 3){
-      document.getElementById('p3').style.setProperty("display","none","important");
+      document.getElementById('p3').classList.remove('show');
+      document.getElementById('p3').classList.add('hide');
     }else{
-      document.getElementById('p4').style.setProperty("display","none","important");
+      document.getElementById('p4').classList.remove('show');
+      document.getElementById('p4').classList.add('hide');
     }
   }
 }
