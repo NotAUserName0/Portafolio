b405d9f0d5fceb3ef9ad5993ee2ea180b504e737
Fermin Jimenez <dill2897@gmail.com>
2023-08-03 01:20:41 -0600

    solo pc

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'portafolio';
+  variableControlMenu: boolean = false
+
+  /* Manejo de evento del menu */
+  @HostListener('document:click', ['$event'])
+  outOfContext(event: Event) {
+    const target = event.target as HTMLElement;
+    const menu = document.getElementById("menu_btn")
+    if (!menu?.contains(target) && this.variableControlMenu) {
+      this.closeMenu()
+    }
+  }
+
+  mostrarMenu(){
+    this.variableControlMenu = !this.variableControlMenu
+    document.getElementById("menu")?.classList.toggle("show")
+  }
+
+  closeMenu() {
+    this.variableControlMenu = false
+    document.getElementById("menu")?.classList.toggle("show")
+  }
 }
